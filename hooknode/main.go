002d672878f0960a1e243e36faf989f9909cc997
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-08-02 13:32:41 -0400

    First pass to do pow in lambda

--- hooknode/main.go
+++ hooknode/main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/oysterprotocol/lambda-node/hooknode/services"
 )
 
 type Response struct {
 	Message string `json:"message"`
 }
 
-func Handler() (Response, error) {
-	return Response{
-		Message: "Go Serverless v1.0! Your function executed successfully!",
-	}, nil
+type hooknodeReq struct {
+	Provider string               `json:"provider"`
+	Chunks   []services.IotaChunk `json:"chunks"`
+}
+
+func handler(req hooknodeReq) (Response, error) {
+	// TODO: Validate params.
+
+	if err := services.AttachAndBroadcast(req.Provider, &req.Chunks); err != nil {
+		return Response{Message: err.Error()}, err
+	}
+
+	return Response{Message: "Success!"}, nil
 }
 
 func main() {
-	lambda.Start(Handler)
+	lambda.Start(handler)
 }
---
+++ hooknode/services/iota.go
@@ -0,0 +1,53 @@
+package services
+
+import "github.com/iotaledger/giota"
+
+const seed = giota.Trytes("OYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRL")
+const security = 1
+const value = int64(0)
+const depth = int64(1)
+const mwm = int64(9)
+
+type IotaChunk struct {
+	Address string `json:"address"`
+	Value   int    `json:"value"`
+	Message string `json:"message"`
+	Tag     string `json:"tag"`
+}
+
+func AttachAndBroadcast(provider string, chunks *[]IotaChunk) error {
+	// TODO: Add logging to segment so we know what's going on.
+
+	api := giota.NewAPI(provider, nil)
+	oysterTag, _ := giota.ToTrytes("OYSTERGOLANG")
+	_, powFn := giota.GetBestPoW() // Log powName.
+
+	// Map chunks to giota.Transfer
+	trs := make([]giota.Transfer, len(*chunks))
+	for i, chunk := range *chunks {
+		addr, err := giota.ToAddress(chunk.Address)
+		msg, err := giota.ToTrytes(chunk.Message)
+		if err != nil {
+			return nil
+		}
+
+		trs[i] = giota.Transfer{
+			Address: addr,
+			Message: msg,
+			Value:   value,
+			Tag:     oysterTag,
+		}
+	}
+
+	bd, err := giota.PrepareTransfers(api, seed, trs, nil, "", security)
+	if err != nil {
+		return err
+	}
+
+	err = giota.SendTrytes(api, depth, []giota.Transaction(bd), mwm, powFn)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
