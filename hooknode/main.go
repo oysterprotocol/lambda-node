94294535b8ed673430d827ada083b0777144445e
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-08-02 17:13:47 -0400

    Parse JSON body

--- hooknode/main.go
+++ hooknode/main.go
@@ -1,27 +1,33 @@
 package main
 
 import (
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/oysterprotocol/lambda-node/hooknode/services"
 )
-
-type Response struct {
-	Message string `json:"message"`
-}
 
 type hooknodeReq struct {
 	Provider string               `json:"provider"`
 	Chunks   []services.IotaChunk `json:"chunks"`
 }
 
-func handler(req hooknodeReq) (Response, error) {
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// TODO: Validate params.
 
-	if err := services.AttachAndBroadcast(req.Provider, &req.Chunks); err != nil {
-		return Response{Message: err.Error()}, err
+	// Parse request body.
+	var req hooknodeReq
+	if err := json.Unmarshal([]byte(request.Body), &req); err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 200}, nil
 	}
 
-	return Response{Message: "Success!"}, nil
+	// PoW + Broadcast
+	if err := services.AttachAndBroadcast(req.Provider, &req.Chunks); err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 200}, nil
+	}
+
+	return events.APIGatewayProxyResponse{Body: "Success!", StatusCode: 200}, nil
 }
 
 func main() {
