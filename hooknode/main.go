f10fd7d46b6b41438c538b00b92696f220ff0f4e
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-08-17 17:04:56 -0400

    Adds Sentry

--- hooknode/main.go
+++ hooknode/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	raven "github.com/getsentry/raven-go"
 	"github.com/oysterprotocol/lambda-node/hooknode/services"
 )
 
@@ -17,19 +19,25 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	// Parse request body.
 	var req hooknodeReq
 	if err := json.Unmarshal([]byte(request.Body), &req); err != nil {
-		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 200}, nil
+		raven.CaptureError(err, nil)
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, nil
 	}
 
 	// TODO: Validate params.
 
 	// PoW + Broadcast
 	if err := services.AttachAndBroadcast(req.Provider, &req.Chunks); err != nil {
-		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 200}, nil
+		raven.CaptureError(err, nil)
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, nil
 	}
 
 	return events.APIGatewayProxyResponse{Body: "Success!", StatusCode: 200}, nil
 }
 
 func main() {
-	lambda.Start(handler)
+	raven.SetDSN(os.Getenv("SENTRY_DSN"))
+
+	raven.CapturePanic(func() {
+		lambda.Start(handler)
+	}, nil)
 }
