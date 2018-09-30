6e971ae6789679374232aa9307b2001caccb72a6
Philip Zhao <pzhao5@users.noreply.github.com>
2018-09-30 17:15:17 -0400

    Update iota.go

--- hooknode/services/iota.go
+++ hooknode/services/iota.go
@@ -19,7 +19,10 @@ func AttachAndBroadcast(provider string, chunks *[]IotaChunk) error {
 	// TODO: Add logging to segment so we know what's going on.
 
 	api := giota.NewAPI(provider, nil)
-	oysterTag, _ := giota.ToTrytes("OYSTERHOOKNODE")
+	oysterTag, err := giota.ToTrytes("OYSTERHOOKNODE")
+	if err != nil {
+		return err
+	}
 	_, powFn := giota.GetBestPoW() // Log powName.
 
 	// Map chunks to giota.Transfer
