4ff0b03ba7a0498c05b6c52f36f9d7d5a9e589f4
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-10-31 21:39:46 -0500

    skeleton to concurrently fetch from s3"

--- hooknode/services/s3.go
+++ hooknode/services/s3.go
@@ -1,21 +1,38 @@
 package services
 
-import "github.com/oysterprotocol/lambda-node/hooknode/types"
+import (
+	"sync"
+
+	"github.com/oysterprotocol/lambda-node/hooknode/types"
+)
 
 type S3 struct{}
 
 // ChunkStore interface
 func (i *S3) AdaptReqChunks(chunks []types.ReqChunk) []types.IotaChunk {
+	var sem sync.WaitGroup
+	sem.Add(len(chunks)) // Semaphore the size of the number of chunks.
+
 	result := make([]types.IotaChunk, len(chunks))
 	// TODO: Fetch Message from s3
 	for i, reqChk := range chunks {
-		result[i] = types.IotaChunk{
-			Address: reqChk.Address,
-			Value:   reqChk.Value,
-			Message: reqChk.Message, // This is null, need to fetch!
-			Tag:     reqChk.Tag,
-		}
+		go func(i int, reqChk types.ReqChunk) {
+			result[i] = types.IotaChunk{
+				Address: reqChk.Address,
+				Value:   reqChk.Value,
+				Message: fetchMsg(reqChk.StoreUrl), // This is null, need to fetch!
+				Tag:     reqChk.Tag,
+			}
+
+			sem.Done()
+		}(i, reqChk)
 	}
 
+	sem.Wait()
+
 	return result
+}
+
+func fetchMsg(url string) string {
+	return url // TODO: Actually fetch from s3
 }
